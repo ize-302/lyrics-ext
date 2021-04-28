9ef09d7fd1de033f92fbec0ebdfe11f1071fde1a
ozorku <adavizeozorku@gmail.com>
2021-04-28 17:11:20 +0100

    A new beginning :tada:

--- src/background/index.js
+++ src/background/index.js
@@ -1,9 +1,13 @@
 // If your extension doesn't need a background script, just leave this file empty
-
-messageInBackground();
+import { getSong } from "genius-lyrics-api";
 
 // This needs to be an export due to typescript implementation limitation of needing '--isolatedModules' tsconfig
-export function messageInBackground() {
-  console.log('I can run your javascript like any other code in your project');
-  console.log('just do not forget, I cannot render anything !');
+export async function fetchSong(query) {
+  const response = await getSong({
+    apiKey: process.env.REACT_APP_ACCESS_TOKEN, // get access token from genius.com
+    title: query,
+    artist: query,
+    optimizeQuery: true,
+  });
+  return response;
 }
--- src/options/Options.js
+++ src/options/Options.js
@@ -1,24 +1,100 @@
-import logo from '../logo.svg';
-import './Options.css';
+import React from "react";
+import {
+  Box,
+  Image,
+  Text,
+  Input,
+  Spacer,
+  Spinner,
+  VStack,
+  HStack,
+  Divider,
+} from "@chakra-ui/react";
+import { fetchSong } from "../background";
+import logo from "../logo.png";
 
 function Options() {
+  const [song, setsong] = React.useState("");
+  const [lyrics, setlyrics] = React.useState([]);
+  const [query, setquery] = React.useState("");
+  const [loading, setloading] = React.useState(false);
+
+  const fireUp = () => {
+    setloading(true);
+    setsong("");
+    fetchSong(query).then((song) => {
+      setsong(song);
+      let lyrics = song.lyrics;
+      lyrics = lyrics.split("\n");
+      setlyrics(lyrics);
+      setloading(false);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/options/Options.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      background="#111"
+      minWidth="400px"
+      maxH="500px"
+      height="100%"
+      overflowY="scroll"
+      padding={5}
+    >
+      <HStack>
+        <Image src={logo} width="30px" />
+        <Input
+          size="sm"
+          focusBorderColor="#47c7fe"
+          borderWidth="1px"
+          autoFocus={true}
+          background="#222"
+          variant="filled"
+          placeholder="Search.."
+          onChange={(e) => setquery(e.target.value)}
+          onKeyPress={(e) => {
+            e.key === "Enter" && fireUp();
+          }}
+          _hover={{}}
+          color="white"
+          disabled={loading}
+        />
+      </HStack>
+
+      <Divider
+        display={loading || song ? "block" : "none"}
+        borderColor="#222"
+        marginTop={5}
+      />
+
+      {/* loading */}
+      {loading && (
+        <VStack marginY={8}>
+          <Spinner size="xl" thickness="4px" color="#47c7fe" />
+        </VStack>
+      )}
+      {/* result */}
+      {song && (
+        <Box>
+          <Image src={song.albumArt} marginY={5} width="100px" />
+          <Box marginBottom={10}>
+            {lyrics.map((line, index) => (
+              <Text key={index} color="white" fontSize="14px">
+                {line === "" ? <Spacer marginY={4} /> : line}
+              </Text>
+            ))}
+          </Box>
+
+          <HStack justifyContent="space-between">
+            <a rel="noreferrer" target="_blank" href="https://genius.com">
+              <Text color="#ffe603">Powered by Genius</Text>
+            </a>
+            <a rel="noreferrer" target="_blank" href="https://genius.com">
+              <Text color="#fff">Source code</Text>
+            </a>
+          </HStack>
+        </Box>
+      )}
+    </Box>
   );
 }
 
